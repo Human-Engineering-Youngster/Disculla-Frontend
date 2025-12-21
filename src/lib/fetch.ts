06fd19207afce5e45ae035247bef82504ba189d7
kmjak <tymkoron1221@icloud.com>
2025-12-21 18:31:41 +0900

    refactor: 命名やバグの修正
    - schemaが複数形になっていたので単数形に変更
    - try catchに処理の追加
    - customFetch以外のoptionsにデフォルト値がなかったので変更
    - 不要なawaitの削除

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 type FetchProps<T> = {
-  schemas: z.ZodType<T>;
+  schema: z.ZodType<T>;
   url: string;
   options?: RequestInit;
 };
@@ -9,13 +9,13 @@ type FetchProps<T> = {
 /**
  * 共通のフェッチ関数
  * @param {object} props - 引数オブジェクト
- * @param {z.ZodType<T>} props.schemas - Zodスキーマ
+ * @param {z.ZodType<T>} props.schema - Zodスキーマ
  * @param {string} props.url - APIエンドポイントのURL
  * @param {RequestInit} [props.options] - フェッチオプション
  * @returns {Promise<T>} - パースされたデータ
  * @throws {Error} - フェッチエラーまたはパースエラー
  */
-export const customFetch = async <T>({ schemas, url, options = {} }: FetchProps<T>): Promise<T> => {
+export const customFetch = async <T>({ schema, url, options = {} }: FetchProps<T>): Promise<T> => {
   try {
     const response = await fetch(url, {
       ...options,
@@ -31,8 +31,12 @@ export const customFetch = async <T>({ schemas, url, options = {} }: FetchProps<
 
     const result = await response.json();
 
-    return schemas.parse(result.data);
+    return schema.parse(result);
   } catch (error) {
+    if (process.env.NODE_ENV === "development") {
+      console.error("Fetch error:", error);
+    }
+
     throw error;
   }
 };
@@ -40,15 +44,15 @@ export const customFetch = async <T>({ schemas, url, options = {} }: FetchProps<
 /**
  * バックエンドと直接通信を行うフェッチ関数（Route Handlerでは動かない）
  * @param {object} props - 引数オブジェクト
- * @param {z.ZodType<T>} props.schemas - Zodスキーマ
+ * @param {z.ZodType<T>} props.schema - Zodスキーマ
  * @param {string} props.url - APIエンドポイントのURL
  * @param {RequestInit} [props.options] - フェッチオプション
  * @returns {Promise<T>} - パースされたデータ
  * @throws {Error} - フェッチエラーまたはパースエラー
  */
-export async function backendFetch<T>({ schemas, url, options }: FetchProps<T>): Promise<T> {
-  return await customFetch({
-    schemas,
+export async function backendFetch<T>({ schema, url, options = {} }: FetchProps<T>): Promise<T> {
+  return customFetch({
+    schema,
     url,
     options: { ...options, credentials: "include" },
   });
@@ -57,15 +61,15 @@ export async function backendFetch<T>({ schemas, url, options }: FetchProps<T>):
 /**
  * Route Handlerと直接通信を行うフェッチ関数
  * @param {object} props - 引数オブジェクト
- * @param {z.ZodType<T>} props.schemas - Zodスキーマ
+ * @param {z.ZodType<T>} props.schema - Zodスキーマ
  * @param {string} props.url - APIエンドポイントのURL
  * @param {RequestInit} [props.options] - フェッチオプション
  * @returns {Promise<T>} - パースされたデータ
  * @throws {Error} - フェッチエラーまたはパースエラー
  */
-export async function routeFetch<T>({ schemas, url, options }: FetchProps<T>): Promise<T> {
-  return await customFetch({
-    schemas,
+export async function apiFetch<T>({ schema, url, options = {} }: FetchProps<T>): Promise<T> {
+  return customFetch({
+    schema,
     url,
     options: { ...options, credentials: "same-origin" },
   });
